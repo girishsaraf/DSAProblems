f69ab393ab7f44a2890bea20e07d37c92c95a76d
Girish N. Saraf <38237037+girish03@users.noreply.github.com>
2022-04-10 18:46:58 -0400

    Time: 46 ms (24.18%), Space: 13.9 MB (27.87%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -1,14 +1,19 @@
 class Solution(object):
     def calPoints(self, ops):
         ans = []
+        s = 0
         for op in ops:
             if op == 'C':
+                s -= ans[-1]
                 ans = ans[:-1]
             elif op == 'D':
+                s += 2*ans[-1]
                 ans.append(2*ans[-1])
             elif op == '+':
+                s += ans[-1] + ans[-2]
                 ans.append(ans[-1] + ans[-2])
             else:
+                s += int(op)
                 ans.append(int(op))
-        return sum(ans)
+        return s
         
